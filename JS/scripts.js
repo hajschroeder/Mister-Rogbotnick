e795b53ff2c7875c6911b81672787240f75adf7a
Hans Schroeder <hajschroeder@gmail.com>
2022-02-11 11:01:45 -0800

    all business logic tests are passing, dummy replacement values have been changed to reflect the requirements of the prompt

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,10 +9,16 @@ function userInput(number){
     outputArray.push(i.toString())
   }
   outputArray.forEach(function(element, index){
-    if (element.includes(3))
-    console.log(outputArray.splice(index, 1, "x"))
-  })
-  return outputArray;
+    if (element.includes(3)) {
+    (outputArray.splice(index, 1, "Won't you be my neighbor"))
+    } else if(element.includes(2)){
+      (outputArray.splice(index, 1, "Boop"))
+    }else if(element.includes(1)){
+      (outputArray.splice(index, 1, "Beep"))
+    }
+    return outputArray;
+  }) 
+  console.log(outputArray)
   
 }
 
