a321247926ca6557fa344aa0d9b3f4bb30df418f
Hans Schroeder <hajschroeder@gmail.com>
2022-02-11 12:18:46 -0800

    include first attempts at User Logic (not currently working)

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,34 +1,30 @@
 //Business Logic
 function userInput(number){
-  const notValid="This Input is not valid"
-  if (number.length === 0){
-    return notValid
-  }
-  let outputArray=[];
-  for(let i=0; i<=number; i++){
-    outputArray.push(i.toString())
-  }
-  outputArray.forEach(function(element, index){
-    if (element.includes(3)) {
+
+let outputArray=[];
+for(let i=0; i<=number; i++){
+  outputArray.push(i.toString())
+}
+outputArray.forEach(function(element, index){
+  if (element.includes(3)) {
     (outputArray.splice(index, 1, "Won't you be my neighbor"))
-    } else if(element.includes(2)){
-      (outputArray.splice(index, 1, "Boop"))
-    }else if(element.includes(1)){
-      (outputArray.splice(index, 1, "Beep"))
-    }
-    return outputArray;
-  }) 
-  console.log(outputArray)
-  
+  } else if(element.includes(2)){
+    (outputArray.splice(index, 1, "Boop"))
+  }else if(element.includes(1)){
+    (outputArray.splice(index, 1, "Beep"))
+  }
+  return outputArray;
+}) 
+console.log(outputArray)
+
 }
 
-// function misterRogers(outputArray){
-//   let resultArray=[]
-//   outputArray.forEach(function(element){
-//     if (element.includes(3)){
-//       resultArray.push("x")
-//     }
-//   })
+//User Logic
 
-// }
-
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
+    const inputMister=$("input#numberInput").val()
+    $("#numberInput").html(inputMister)
+  })
+})
