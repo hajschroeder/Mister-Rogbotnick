4f3b2e0281a83e98fdb26f3a9c1a29bed40205fc
Hans Schroeder <hajschroeder@gmail.com>
2022-02-17 11:42:30 -0800

    change the names of some functions and variables to make their purpose more clear, fix the missplaced parenthis to make the output display on the page

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,33 +1,27 @@
 //Business Logic
-function userInput(number){
-
+function translateInputBeepBoop(number){
 let outputArray=[];
-for(let i=0; i<=number; i++){
-  outputArray.push(i.toString());
-}
-outputArray.forEach(function(element, index){
-  if (element.includes(3)) {
-    (outputArray.splice(index, 1, "Won't you be my neighbor"));
-  } else if(element.includes(2)){
-    (outputArray.splice(index, 1, "Boop"));
-  } else if(element.includes(1)){
-    (outputArray.splice(index, 1, "Beep"));
+  for(let i=0; i<=number; i++){
+    outputArray.push(i.toString());
   }
+  outputArray.forEach(function(element, index){
+    if (element.includes(3)) {
+      outputArray.splice(index, 1, "Won't you be my neighbor ");
+    } else if(element.includes(2)){
+      outputArray.splice(index, 1, "Boop ");
+    } else if(element.includes(1)){
+      outputArray.splice(index, 1, "Beep ");
+    }
+  });
   return outputArray;
-});
-  console.log(outputArray);
-  //returns desired output
 }
 
 // User Logic
 $(document).ready(function(){
   $("form#form").submit(function(event){
     event.preventDefault();
-    const output=$("input#numberInput").val();
-    $("#result").html(userInput(output));
-
-    console.log(userInput(output));
-    // returns as 'undefined'
+    const userNumber=$("input#numberInput").val();
+    $("#result").html(translateInputBeepBoop(userNumber));
   });
 });
 
